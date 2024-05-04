566d5a92aa8611519c614c97b830304682d3ee2f
Max Pavet <maxpavet.pro@gmail.com>
2024-05-04 16:39:52 +0200

    Ajoutez des fichiers projet.

---
+++ OpenGLTest/boneInfo.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include<glm.hpp>
+
+struct BoneInfo
+{
+	int id;
+	glm::mat4 offset;
+
+};
+#pragma once
---
+++ VulkanTest/compile.bat
@@ -0,0 +1,3 @@
+C:/Projects/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe vShader.vert -o vert.spv
+C:/Projects/Libraries/VulkanSDK/1.3.261.1/Bin/glslc.exe fShader.frag -o frag.spv
+pause
